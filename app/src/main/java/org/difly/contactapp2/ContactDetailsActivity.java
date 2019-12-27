eb675e5ba8e3fd66fe0ad22b61d69dbd90586919
difly <kryshen.yaroslav@gmail.com>
2019-12-27 04:26:28 +0200

    Create new activity to details view

---
+++ app/src/main/java/org/difly/contactapp2/ContactDetailsActivity.java
@@ -0,0 +1,17 @@
+package org.difly.contactapp2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class ContactDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact_details);
+
+    }
+}
