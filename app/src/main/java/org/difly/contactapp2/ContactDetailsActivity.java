0fe352180e0c24acd19c262b47e630271a0ec4a7
difly <kryshen.yaroslav@gmail.com>
2019-12-29 00:23:26 +0200

    add reference between contact value and view on screen

--- app/src/main/java/org/difly/contactapp2/ContactDetailsActivity.java
+++ app/src/main/java/org/difly/contactapp2/ContactDetailsActivity.java
@@ -15,12 +15,27 @@ import org.difly.contactapp2.entity.Contact;
 public class ContactDetailsActivity extends AppCompatActivity {
     public static final String CONTACT_KEY = "contact";
 
+    TextView textViewValueOfFirstName;
+    TextView textViewValueOfLastName;
+    TextView textViewValueOfPhoneNumber;
+    TextView textViewValueOfPhoneType;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact_details);
 
+        textViewValueOfFirstName = findViewById(R.id.text_firstname_value);
+        textViewValueOfLastName = findViewById(R.id.text_lastname_value);
+        textViewValueOfPhoneNumber = findViewById(R.id.text_phonenumber_value);
+        textViewValueOfPhoneType = findViewById(R.id.text_phonetype_value);
+
         Contact contact = (Contact) getIntent().getSerializableExtra(CONTACT_KEY);
+
+        textViewValueOfFirstName.setText(contact.getFirstname());
+        textViewValueOfLastName.setText(contact.getLastname());
+        textViewValueOfPhoneNumber.setText(contact.getPhonenumber());
+        textViewValueOfPhoneType.setText(contact.getPhonetype());
 
         FloatingActionButton fab = findViewById(R.id.fabEdit);
         fab.setOnClickListener(new View.OnClickListener() {
